bc55ef7d803c27df99f70ca5d6c86bb9e9b1ca90
Ganga Praju <gangapraiju4@gmail.com>
2025-04-20 19:44:14 +0530

    Add initial Python files

---
+++ arithmetic_operations.py
@@ -0,0 +1,43 @@
+a=10
+b=5
+#ADD..
+# This is the same as Addition in Python 2
+# In Python 3, + is used for addition
+print("Addition of a and b is",a+b,"and of b and a is",b+a)
+print("\n")
+#Subtraction..
+# This is the same as Subtraction in Python 2
+# In Python 3, - is used for subtraction
+print("Subtraction of a and b is",a-b,"and of b and a is",b-a)
+print("\n")
+#Multiplication..
+# This is the same as Multiplication in Python 2
+# In Python 3, * is used for multiplication
+print("Multiplication of a and b is",a*b,"and of b and a is",b*a)
+print("\n")
+#Division..
+# This is the same as Float Division in Python 2
+# In Python 3, / is used for float division
+print("Division of a and b is",a/b,"and of b and a is",b/a)
+print(" ")
+#Modulus..
+# This is the same as Modulus in Python 2
+# In Python 3, % is used for modulus
+print("Modulus of a and b is",a%b,"and of b and a is",b%a)
+print(" ")
+#Exponentiation..
+# This is the same as Power in Python 2
+# In Python 3, ** is used for exponentiation
+print("Exponent of a and b is",a**b,"and of b and a is",b**a)
+print(" ")
+#Floor Division..
+# This is the same as Integer Division in Python 2
+# In Python 3, // is used for floor division
+print("Floor division of a and b is",a//b,"and of b and a is",b//a)
+print(" ")
+#Integer Division..
+# This is the same as Floor Division in Python 3
+print("Integer division of a and b is",a//b,"and of b and a is",b//a)
+
+
+

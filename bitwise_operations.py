5a1c61719c21eb374b78c82c02a1fe060b1020f1
Ganga Praju <gangapraiju4@gmail.com>
2025-04-20 20:16:04 +0530

    Add examples of bitwise operations (AND, OR, XOR, NOT)

--- bitwise_operations.py
+++ bitwise_operations.py
@@ -4,25 +4,19 @@ a=2
 b=3
 #BITWISE OPERATIONS
 #AND.....
-print("The First Operation in bitwise is AND")
 print("Bitwise AND of a and b is",a&b,"and of b and a is",b&a)
 print("")
 #OR.....
-print("The Second Operation in bitwise is OR")
 print("Bitwise OR of a and b is",a|b,"and of b and a is",b|a)
 print("")
 #XOR.....
-print("The Third Operation in bitwise is XOR")
 print("Bitwise XOR of a and b is",a^b,"and of b and a is",b^a)
 print("")
 #NOT.....
-print("The Fourth Operation in bitwise is NOT")
 print("Bitwise NOT of a is",~a,"and of b is",~b)
 print("")
 #Left Shift.....
-print("The Fifth Operation in bitwise is Left Shift")
 print("Left shift of a by 2 is",a<<2,"and of b is",b<<2)
 print("")
 #Right Shift.....
-print("The Sixth Operation in bitwise is Right Shift")
 print("Right shift of a by 2 is",a>>2,"and of b is",b>>2)

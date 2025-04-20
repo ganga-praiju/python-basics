2b6419ec84b65218a6fba3368aacd06a39f644f3
Ganga Praju <gangapraiju4@gmail.com>
2025-04-20 20:29:22 +0530

    Add arithmetic operations for basic calculator

--- arithmetic_operations.py
+++ arithmetic_operations.py
@@ -7,25 +7,18 @@ a=10
 b=5
 #ADD..
 print("Addition of a and b is",a+b,"and of b and a is",b+a)
-print("")
 #Subtraction..
 print("Subtraction of a and b is",a-b,"and of b and a is",b-a)
-print("")
 #Multiplication..
 print("Multiplication of a and b is",a*b,"and of b and a is",b*a)
-print("")
 #Division..
 print("Division of a and b is",a/b,"and of b and a is",b/a)
-print(" ")
 #Modulus..
 print("Modulus of a and b is",a%b,"and of b and a is",b%a)
-print(" ")
 #Exponentiation..
 print("Exponent of a and b is",a**b,"and of b and a is",b**a)
-print(" ")
 #Floor Division..
 print("Floor division of a and b is",a//b,"and of b and a is",b//a)
-print(" ")
 #Integer Division..
 print("Integer division of a and b is",a//b,"and of b and a is",b//a)
 
